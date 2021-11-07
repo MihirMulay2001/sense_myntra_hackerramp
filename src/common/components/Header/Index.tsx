b29359a9e8d1dd7c9185ffd14568033dacb9efb5
Mihir Mulay <55177881+MihirMulay2001@users.noreply.github.com>
2021-11-08 02:02:18 +0530

    added cart icon

--- src/common/components/Header/Index.tsx
+++ src/common/components/Header/Index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
+import { HiShoppingCart } from 'react-icons/hi'
 
 
 function Index() {
     return (
         <div className={styles.header}>
             <Link to="/Cart">
-                Cart
+                <HiShoppingCart />
             </Link>
         </div>
     )
